a21556d649db91672c26a45266ea917eaae895c2
Katerina Sidorenko <ekaterinasidorenko0914@gmail.com>
2025-11-23 18:45:38 +0200

    Update 000007_job_submit.up.sql

--- backend/src/migrations/000007_job_submit.up.sql
+++ backend/src/migrations/000007_job_submit.up.sql
@@ -1,15 +1,3 @@
-
--- type JobSubmit struct {
--- 	ID               uint      `json:"id" gorm:"primaryKey;autoIncrement"`
--- 	JobApplicationID uint      `json:"job_application_id" gorm:"not null"`
--- 	SubmittedAt      time.Time `json:"submitted_at" gorm:"autoCreateTime"`
--- 	Description      string    `json:"description" gorm:"not null"`
--- 	FileId           uint      `json:"file_id" gorm:"not null"`
--- 	CreatedAt        time.Time `json:"created_at" gorm:"autoCreateTime"`
--- 	UpdatedAt        time.Time `json:"updated_at" gorm:"autoUpdateTime"`
--- }
-
-
 CREATE TABLE job_submits (
     id SERIAL PRIMARY KEY,
     job_application_id INT NOT NULL,
